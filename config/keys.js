d00ea20e14d80a7ef7434958cb575d9699a506be
Robert James Bowes <up666724@myport.ac.uk>
2017-11-19 20:18:37 +0000

    Further troubleshooting heroku deployment

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,5 @@
-//keys.js
-
 if (process.env.NODE_ENV === "production") {
-  //in prod = return prod keys
   module.exports = require("./prod");
 } else {
-  //in dev = return dev keys
   module.exports = require("./dev");
 }
