514e11ce0f568b6179553c10c0a1a800e6ed862a
Robert James Bowes <up666724@myport.ac.uk>
2017-11-19 19:28:48 +0000

    finish auth flow

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//keys.js
+
+if (process.env.NODE_ENV === "production") {
+  //in prod = return prod keys
+  module.exports = require("./prod");
+} else {
+  //in dev = return dev keys
+  module.exports = require("./dev");
+}
